05b8c8af54a222ba08c871c30e2f6e72de65c053
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-04-22 19:23:07 +0800

    feat: add object-id library

--- packages/object-id/src/lib/object-id.ts
+++ packages/object-id/src/lib/object-id.ts
@@ -1,5 +1,11 @@
+import bs58 from 'bs58';
 import * as crypto from 'crypto';
 import { ObjectType } from './object-type';
+
+const Prefix: Partial<{ [key in ObjectType]: string }> = {
+  [ObjectType.ACCOUNT]: 'acc',
+  [ObjectType.PRODUCT]: 'pro',
+};
 
 export default class ObjectId {
   private readonly value: string | Buffer;
@@ -8,20 +14,30 @@ export default class ObjectId {
     this.value = value || crypto.randomBytes(8);
   }
 
-  static generate(type: ObjectType) {
-    return;
+  static generate(type: ObjectType): ObjectId {
+    const buffer = Buffer.concat([Buffer.from([type]), crypto.randomBytes(15)]);
+    return new ObjectId(buffer);
   }
 
-  static from(value: string) {
-    return;
+  toString(): string {
+    const buffer =
+      typeof this.value === 'string'
+        ? Buffer.from(bs58.decode(this.value))
+        : this.value;
+
+    const hex = buffer.toString('hex');
+    return `${Prefix[this.type]}_${hex}`;
   }
 
-  toString() {
-    return;
+  static from(value: string): ObjectId {
+    const [, encoded] = value.split('_');
+    return new ObjectId(Buffer.from(encoded, 'hex'));
   }
 
-  toBuffer() {
-    return;
+  toBuffer(): Buffer {
+    return typeof this.value === 'string'
+      ? Buffer.from(bs58.decode(this.value))
+      : this.value;
   }
 
   get type(): ObjectType {
