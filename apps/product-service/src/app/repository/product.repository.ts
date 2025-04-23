616a7972632f75670934b25806d6a385c7337f69
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-04-24 02:31:26 +0800

    feat: add cursor field for product repository

--- apps/product-service/src/app/repository/product.repository.ts
+++ apps/product-service/src/app/repository/product.repository.ts
@@ -1,8 +1,13 @@
 import { Connection, Schema } from 'mongoose';
 import { MongooseRepository, Repository } from '@lib/repository';
 import { Product } from '@lib/types';
+import * as crypto from 'crypto';
 
 export type ProductRepository = Repository<Product>;
+
+function hash(input: string | Buffer): Buffer {
+  return crypto.createHash('sha256').update(input).digest();
+}
 
 export function ProductRepositoryFactory(connection: Connection) {
   const schema = new Schema({
@@ -23,6 +28,10 @@ export function ProductRepositoryFactory(connection: Connection) {
       type: String,
       required: true,
     },
+    cursor: {
+      type: Buffer,
+      required: true,
+    },
     dateTimeCreated: {
       type: Date,
       default: () => new Date(),
@@ -31,6 +40,17 @@ export function ProductRepositoryFactory(connection: Connection) {
       type: Date,
       default: () => new Date(),
     },
+  });
+
+  schema.pre('validate', function (next) {
+    if (!this.cursor) {
+      const dateTime = this.dateTimeCreated || new Date();
+      const id = this._id;
+      const timeBuffer = Buffer.alloc(6);
+      timeBuffer.writeUIntBE(dateTime.getTime(), 0, 6);
+      this.cursor = Buffer.concat([timeBuffer, hash(id).slice(0, 4)]);
+    }
+    next();
   });
 
   return new MongooseRepository<Product>(connection, 'Product', schema);
