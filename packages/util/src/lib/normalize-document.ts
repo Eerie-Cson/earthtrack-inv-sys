3c132dbb8d66407d5a5684e8ebd3c710805e845b
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-04-24 02:31:03 +0800

    feat: convert cursor to string in normalize-document

--- packages/util/src/lib/normalize-document.ts
+++ packages/util/src/lib/normalize-document.ts
@@ -1,6 +1,12 @@
 import { ObjectId } from '@lib/object-id';
 export function normalizeDocument<T extends { id: ObjectId }>(
-  doc: T
-): Omit<T, 'id'> & { id: string } {
-  return { ...doc, id: doc.id.toString() };
+  doc: T & { cursor?: Buffer }
+): Omit<T, 'id'> & { id: string; cursor?: string | Buffer } {
+  const rawCursor = doc.cursor;
+
+  return {
+    ...doc,
+    ...(rawCursor ? { cursor: rawCursor.toString('base64') } : {}),
+    id: doc.id.toString(),
+  };
 }
