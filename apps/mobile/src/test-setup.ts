a61700d5c96013f0080f891e7e6efa6f6b921228
eerie-cson <ericson.sacdalan.4@gmail.com>
2025-04-29 08:27:02 +0800

    feat: use @testing-library/reac instead of "@testing-library/react-hooks": "^8.0.1",

--- apps/mobile/src/test-setup.ts
+++ apps/mobile/src/test-setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-native/extend-expect';
+import 'react-native-gesture-handler/jestSetup';
+
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
+  Reanimated.default.call = jest.fn();
+  return Reanimated;
+});
+
+jest.mock('react-native-gesture-handler', () => {
+  const GestureHandler = require('react-native-gesture-handler/jestSetup');
+  return GestureHandler;
+});
+
+jest.spyOn(console, 'error').mockImplementation(jest.fn());
+jest.spyOn(console, 'log').mockImplementation(jest.fn());
